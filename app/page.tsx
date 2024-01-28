eb21cf4da88ce77de4865b4af165900bf926b18a
yakinmohamed <allagui_fawzi@outlook.fr>
2024-01-28 21:55:06 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <p className="text-3xl font-medium text-sky-700"> hello world</p>
+    </div>
+  );
+}
