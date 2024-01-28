ef08c622a81d7b7f39aef66c3f6928ae32bcc99b
yakinmohamed <allagui_fawzi@outlook.fr>
2024-01-28 22:54:54 +0100

    layout.tsx for the (auth)

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-      <p className="text-3xl font-medium text-sky-700"> hello world</p>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
     </div>
-  );
+  )
 }
