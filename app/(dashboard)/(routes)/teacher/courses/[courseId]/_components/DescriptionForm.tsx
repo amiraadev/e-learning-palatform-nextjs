3fc744011a60f40665404ff6b6b180f2ff0a3df8
yakinmohamed <allagui_fawzi@outlook.fr>
2024-01-30 20:08:35 +0100

    DescriptionForm.tsx

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/DescriptionForm.tsx
@@ -0,0 +1,109 @@
+/** @format */
+
+"use client";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import toast from "react-hot-toast";
+import { Pencil } from "lucide-react";
+
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+
+interface DescriptionFormProps {
+	initialData: {
+		description: string ;
+	};
+	courseId: string;
+}
+
+const formSchema = z.object({
+	description: z.string().min(1, {
+		message: "Description is required",
+	}),
+});
+const DescriptionForm: React.FC<DescriptionFormProps> = ({ initialData, courseId }) => {
+	const [isEditing, setIsEditing] = useState(false);
+	const router = useRouter();
+	const toggleEdit = () => setIsEditing((current) => !current);
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: initialData,
+	});
+
+	const { isSubmitting, isValid } = form.formState;
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		try {
+			await axios.patch(`/api/courses/${courseId}`, values);
+			toast.success("Course updated");
+			toggleEdit();
+			router.refresh();
+		} catch (error) {
+			toast.error("Something went wrong!");
+		}
+	};
+
+	return (
+		<div className='mt-6 border bg-slate-100 rounded-md p-4'>
+			<div className='font-medium flex items-center justify-between'>
+				Course Description
+				<Button onClick={toggleEdit} variant='ghost'>
+					{isEditing ? (
+                        <>Cancel</>
+					
+					) : (
+						<>
+							<Pencil className='h-4 w-4 mr-2' />
+							Edit description
+						</>
+					)}
+				</Button>
+			</div>
+			{!isEditing && <p className={cn("text-sm mt-2",!initialData.description && "text-slate-500 italic")}>{initialData.description || "No description"}</p>}
+			{isEditing && (
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className='space-y-4 mt-4'>
+						<FormField
+							control={form.control}
+							name='description'
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Textarea
+											disabled={isSubmitting}
+											placeholder="e.g 'This course is about...'"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<div className='flex items-center gap-x-2'>
+							<Button disabled={!isValid || isSubmitting} type='submit'>
+								Save
+							</Button>
+						</div>
+					</form>
+				</Form>
+			)}
+		</div>
+	);
+};
+
+export default DescriptionForm;
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -7,6 +7,7 @@ import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import React from "react";
 import TitleForm from "./_components/TitleForm";
+import DescriptionForm from "./_components/DescriptionForm";
 
 const CourseIdPAge = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -35,6 +36,9 @@ const CourseIdPAge = async ({ params }: { params: { courseId: string } }) => {
 
 	const completionText = `(${completedFields}/${totalFields})`;
 
+	// console.log(course);
+	
+
 	return (
 		<div className='p-6'>
 			<div className='flex items-center justify-between'>
@@ -52,6 +56,10 @@ const CourseIdPAge = async ({ params }: { params: { courseId: string } }) => {
                         </h2>
                     </div>
                     <TitleForm 
+                     initialData={course}
+                     courseId={course.id}
+                    />
+                    <DescriptionForm  
                      initialData={course}
                      courseId={course.id}
                     />
