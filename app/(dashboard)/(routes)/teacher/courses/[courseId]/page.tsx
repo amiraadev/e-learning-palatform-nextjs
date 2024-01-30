a25bb47cb127cc9d248e81d63dcf76a10575ed5c
yakinmohamed <allagui_fawzi@outlook.fr>
2024-01-30 16:00:11 +0100

    [courseId]

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CourseIdPAge = ({params}:{
+    params:{courseId:string}
+}) => {
+  return (
+    <div>CourseId:{params.courseId}</div>
+  )
+}
+
+export default CourseIdPAge
