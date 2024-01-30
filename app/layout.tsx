1ee8b6fca375d1a8e5735bb0d0f21522981eead3
yakinmohamed <allagui_fawzi@outlook.fr>
2024-01-30 13:13:55 +0100

    adding the prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import ToasterProvider from "@/components/providers/toaster-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang='en'>
-				<body className={inter.className}>{children}</body>
+				<body className={inter.className}>
+					<ToasterProvider />
+					{children}
+				</body>
 			</html>
 		</ClerkProvider>
 	);
