81d4fb613a94348d36ff609dc932909db26d078f
yakinmohamed <allagui_fawzi@outlook.fr>
2024-01-30 21:42:32 +0100

    /app/uploadthing/core.ts and route.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,33 +1,28 @@
+import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
  
 const f = createUploadthing();
  
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+const handleAuth = () =>{
+    const { userId } = auth();
+    if(!userId){
+        throw new Error("Unauthorized");
+    }
+    return {
+        userId
+    };
+}
  
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
- 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new Error("Unauthorized");
- 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
- 
-      console.log("file url", file.url);
- 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
-    }),
-} satisfies FileRouter;
+  courseImage:f({image: {maxFileSize:"4KB",maxFileCount:1}})
+                .middleware(()=> handleAuth()  )
+                .onUploadComplete(()=> {}  ),
+  courseAttachment:f(["text", "image","video","audio","pdf"])
+                 .middleware(()=> handleAuth() )
+                 .onUploadComplete(()=> {}  ),
+  chapterVideo:f({video:{maxFileCount:1, maxFileSize:"512GB"}})
+                 .middleware(()=> handleAuth() )
+                 .onUploadComplete(()=> {}  ),
+                } satisfies FileRouter;
  
 export type OurFileRouter = typeof ourFileRouter;
