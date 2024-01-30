b673e3926595d5b1e60cde052cf21bebcbbccfef
yakinmohamed <allagui_fawzi@outlook.fr>
2024-01-30 11:10:27 +0100

    Navbar skeleton

---
+++ app/(dashboard)/_components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='p-4 border-b h-full flex items-center bg-white shadow-sm'>Navbar</div>
+  )
+}
+
+export default Navbar
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,10 +2,14 @@
 
 import React from "react";
 import Sidebar from "./_components/Sidebar";
+import Navbar from "./_components/Navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className='h-full'>
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+        <Navbar />
+      </div>
 			<div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50'>
 				<Sidebar />
 			</div>
