df06c56b28b6294b9dabb45298bbf6c6f695f081
yakinmohamed <allagui_fawzi@outlook.fr>
2024-01-30 13:09:03 +0100

    ToasterProvider

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,73 +1,109 @@
-/** @format */
-
 "use client";
-// "zod" that is directly associated with Next.js :  library used for form validation or schema validation in your Next.js project
+
 import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
 import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+  FormItem,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-	title: z.string().min(1, {
-		message: "title is required",
-	}),
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
 });
+
 const CreatePage = () => {
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			title: "",
-		},
-	});
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: ""
+    },
+  });
 
-	const { isSubmitting, isValid } = form.formState;
-	const onSubmit = (values: z.infer<typeof formSchema>) => {
-		console.log(values);
-	};
-	return (
-		<div className='max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6'>
-			<div>
-				<h1 className='text-2xl'>Name your course</h1>
-				<p className='tex-sm text-slate-600'>
-					What would you like to name your course ? don&apos;t worry you can
-					change this later
-				</p>
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(onSubmit)}
-						className='space-y-8 mt-8'>
-						<FormField
-							control={form.control} // No need for control={form.control} here                 title='title'
-							render={({ field }) => <FormItem>
-                                <FormLabel>
-                                    Course Title
-                                </FormLabel>
-                                <FormControl>
-                                    <Input
-                                      disabled={isSubmitting}
-                                      placeholder="e.g 'Advanced web Development'"
-                                      {...field}
-                                    />
-                                </FormControl>
-                            </FormItem>}
-						/>
-					</form>
-				</Form>
-			</div>
-		</div>
-	);
-};
+  const { isSubmitting, isValid } = form.formState;
 
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/courses", values);
+      router.push(`/teacher/courses/${response.data.id}`);
+      toast.success("Course created");
+    } catch {
+      toast.error("Something went wrong");
+    }
+  }
+
+  return ( 
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">
+          Name your course
+        </h1>
+        <p className="text-sm text-slate-600">
+          What would you like to name your course? Don&apos;t worry, you can change this later.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Course title
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Advanced web development'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button
+                  type="button"
+                  variant="ghost"
+                >
+                  Cancel
+                </Button>
+              </Link>
+              <Button
+                type="submit"
+                disabled={!isValid || isSubmitting}
+              >
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+   );
+}
+ 
 export default CreatePage;
