84f6c37486d7c0e0113cf1e7936dcd23602c6375
yakinmohamed <allagui_fawzi@outlook.fr>
2024-01-30 22:55:23 +0100

    ImageForm.tsx

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ImageForm copy.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ImageForm copy.tsx
@@ -8,7 +8,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import toast from "react-hot-toast";
-import { Pencil } from "lucide-react";
+import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
 
 import {
 	Form,
@@ -22,25 +22,29 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import { Course } from "@prisma/client";
+import Image from "next/image";
+import FileUpload from "@/components/FileUpload";
 
-interface DescriptionFormProps {
+interface ImageFormProps {
 	initialData: Course;
 	courseId: string;
 }
 
 const formSchema = z.object({
-	description: z.string().min(1, {
-		message: "Description is required",
+	imageUrl: z.string().min(1, {
+		message: "Image is required!",
 	}),
 });
-const DescriptionForm: React.FC<DescriptionFormProps> = ({ initialData, courseId }) => {
+const ImageForm: React.FC<ImageFormProps> = ({ initialData, courseId }) => {
 	const [isEditing, setIsEditing] = useState(false);
 	const router = useRouter();
 	const toggleEdit = () => setIsEditing((current) => !current);
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
-		defaultValues: {description:initialData?.description || " "},
+		defaultValues: {
+			imageUrl: initialData?.imageUrl || "",
+		},
 	});
 
 	const { isSubmitting, isValid } = form.formState;
@@ -58,51 +62,56 @@ const DescriptionForm: React.FC<DescriptionFormProps> = ({ initialData, courseId
 	return (
 		<div className='mt-6 border bg-slate-100 rounded-md p-4'>
 			<div className='font-medium flex items-center justify-between'>
-				Course Description
+				Course Image
 				<Button onClick={toggleEdit} variant='ghost'>
-					{isEditing ? (
-                        <>Cancel</>
-					
-					) : (
+					{isEditing && <>Cancel</>}
+					{!isEditing && !initialData.imageUrl && (
+						<>
+							<PlusCircle className='h-4 w-4 mr-2' />
+							Add an image
+						</>
+					)}
+					{!isEditing && initialData.imageUrl &&(
 						<>
 							<Pencil className='h-4 w-4 mr-2' />
-							Edit description
+							Edit Image
 						</>
 					)}
 				</Button>
 			</div>
-			{!isEditing && <p className={cn("text-sm mt-2",!initialData.description && "text-slate-500 italic")}>{initialData.description || "No description"}</p>}
+			{!isEditing && ( !initialData.imageUrl ? (
+				<div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+					<ImageIcon className="h-10 w-10 text-slate-500" />
+				</div>
+			): 
+			<div className="relative aspect-video mt-2">
+				<Image
+				  alt="Upload"
+                  fill
+				  className="object-cover rounded-md"
+				  src={initialData.imageUrl}
+				/>
+			</div>
+			)}
+
+
 			{isEditing && (
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(onSubmit)}
-						className='space-y-4 mt-4'>
-						<FormField
-							control={form.control}
-							name='description'
-							render={({ field }) => (
-								<FormItem>
-									<FormControl>
-										<Textarea
-											disabled={isSubmitting}
-											placeholder="e.g 'This course is about...'"
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<div className='flex items-center gap-x-2'>
-							<Button disabled={!isValid || isSubmitting} type='submit'>
-								Save
-							</Button>
-						</div>
-					</form>
-				</Form>
+				<div>
+					<FileUpload 
+					 endpoint="courseImage"
+					 onChange={(url) =>{
+						if(url){
+							onSubmit({imageUrl:url})
+						}
+					 }}
+					/>
+					<div className="text-xs text-muted-foreground mt-4">
+						16:9 aspect ratio recommended
+					</div>
+				</div>
 			)}
 		</div>
 	);
 };
 
-export default DescriptionForm;
+export default ImageForm;
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,6 +8,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 import TitleForm from "./_components/TitleForm";
 import DescriptionForm from "./_components/DescriptionForm";
+import ImageForm from "./_components/ImageForm copy";
 
 const CourseIdPAge = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -60,6 +61,10 @@ const CourseIdPAge = async ({ params }: { params: { courseId: string } }) => {
                      courseId={course.id}
                     />
                     <DescriptionForm  
+                     initialData={course}
+                     courseId={course.id}
+                    />
+                    <ImageForm  
                      initialData={course}
                      courseId={course.id}
                     />
