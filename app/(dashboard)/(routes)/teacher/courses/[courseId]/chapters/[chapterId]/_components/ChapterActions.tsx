bcb10f15bfde3042e4b43d61c8fd1609bba06cd5
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-02 22:53:35 +0100

    delete a chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
@@ -2,8 +2,15 @@
 
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 import { Trash } from "lucide-react";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+import ConfirmModal from "@/components/modals/ConfirmModal";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 interface ChapterActions {
 	disabled: boolean;
@@ -17,14 +24,37 @@ const ChapterActions = ({
 	chapterId,
 	isPublished,
 }: ChapterActions) => {
-	return <div className="flex items-center gap-x-2">
-        <Button  onClick={()=>{}} disabled={disabled} variant="outline" size="sm">
-            {isPublished ? "Unpublished" : "Published"}
-        </Button>
-        <Button   size="sm">
-            <Trash className="h-4 w-4"/>
-        </Button>
-    </div>;
+    const router = useRouter();
+	const [isLoading, setIsLoading] = useState(false);
+	const onDelete = async () => {
+		try {
+			setIsLoading(true);
+            await axios.delete(`/api/courses/${courseId}/chapter/${chapterId}`)
+            toast.success("Chapter deleted")
+            router.refresh()
+            router.push(`/teacher/courses/${courseId}`)
+		} catch (error) {
+			toast.error("Something went wrong");
+		} finally {
+			setIsLoading(false);
+		}
+	};
+	return (
+		<div className='flex items-center gap-x-2'>
+			<Button
+				onClick={() => {}}
+				disabled={disabled || isLoading}
+				variant='outline'
+				size='sm'>
+				{isPublished ? "Unpublished" : "Published"}
+			</Button>
+			<ConfirmModal onConfirm={onDelete}>
+				<Button size='sm' disabled={isLoading}>
+					<Trash className='h-4 w-4' />
+				</Button>
+			</ConfirmModal>
+		</div>
+	);
 };
 
 export default ChapterActions;
