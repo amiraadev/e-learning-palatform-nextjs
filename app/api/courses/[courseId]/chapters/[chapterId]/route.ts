dfc7c070d6726fda6470b29d93ff68205f2e4d0c
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-02 23:20:26 +0100

    ChapterActions.tsx

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -10,6 +10,82 @@ const { Video } = new Mux(
 	process.env.MUX_TOKEN_SECRET!
 );
 
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { courseId: string; chapterId: string } }
+) {
+	
+	try {
+		const { userId } = auth();
+		if (!userId) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+		const ownCourse = await db.course.findUnique({
+			where: {
+				id: params.courseId,
+				userId,
+			},
+		});
+
+		if (!ownCourse) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+		const chapter = await db.chapter.findUnique({
+			where: {
+				id: params.chapterId,
+				courseId: params.courseId,
+			},
+		});
+		if (!chapter) {
+			return new NextResponse("Not Found", { status: 404 });
+		}
+
+		if (chapter.videoUrl) {
+			const existingMuxData = await db.muxData.findFirst({
+				where: {
+					chapterId: params.chapterId,
+				},
+			});
+
+			if (existingMuxData) {
+				await Video.Assets.del(existingMuxData.assetId);
+				await db.muxData.delete({
+					where: {
+						id: existingMuxData.id,
+					},
+				});
+			}
+		}
+		const deletedChapter = await db.chapter.delete({
+			where: {
+				id: params.chapterId,
+			},
+		});
+
+		const publishedChapterInCourse = await db.chapter.findMany({
+			where: {
+				courseId: params.courseId,
+				isPublished: true,
+			},
+		});
+		if (!publishedChapterInCourse.length) {
+			await db.course.update({
+				where: {
+					id: params.courseId,
+				},
+				data: {
+					isPublished: false,
+				},
+			});
+		}
+
+		return NextResponse.json(deletedChapter)
+	} catch (error) {
+		console.log("[COURSES_ID_DELETE]", error);
+		return new NextResponse("Internal Error", { status: 500 });
+	}
+}
+
 export async function PATCH(
 	req: Request,
 	{ params }: { params: { courseId: string; chapterId: string } }
