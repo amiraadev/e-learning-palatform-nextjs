f0200226d79d8a75731db31b3eca462a701a3686
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-03 20:45:07 +0100

    formatting the price with Intl.NumberFormat

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -46,6 +46,14 @@ export const columns: ColumnDef<Course>[] = [
 				</Button>
 			);
 		},
+        cell:({ row }) => {
+            const price = parseFloat(row.getValue("price") || "0");
+            const formatted = new Intl.NumberFormat("en-Us",{
+                style:"currency",
+                currency:"USD"
+            }).format(price);
+            return <div>{formatted}</div>
+        }
 	},
 	{
 		accessorKey: "isPublished",
