8b72d26a887da21799314bfdca20b6a48d58effc
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-03 22:09:50 +0100

    CategoryItem.tsx

--- app/(dashboard)/(routes)/search/_components/CategoryItem.tsx
+++ app/(dashboard)/(routes)/search/_components/CategoryItem.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+/** @format */
 
-const CategoryItem = () => {
-  return (
-    <div>CategoryItem</div>
-  )
+"use client";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { IconType } from "react-icons/lib";
+
+interface CategoryItemProps {
+	label: string;
+	value?: string;
+	icon?: IconType;
 }
+const CategoryItem = ({ label, value, icon: Icon }: CategoryItemProps) => {
+	return (
+		<button
+			className={cn(
+				"py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition "
+			//   , change style if active
+            )}
+            type="button"
+            >
+			{Icon && <Icon size={20} />}
+			<div className='truncate'>{label}</div>
+		</button>
+	);
+};
 
-export default CategoryItem
+export default CategoryItem;
