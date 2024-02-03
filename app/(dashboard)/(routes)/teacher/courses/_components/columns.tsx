a799b2a387d64ab193916aebd6d3601e06fa6bc2
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-03 19:29:20 +0100

    fetching the courses to the table

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -1,27 +1,23 @@
 "use client"
 
+import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
-}
 
-export const columns: ColumnDef<Payment>[] = [
+
+export const columns: ColumnDef<Course>[] = [
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "title",
+    header: "Title",
   },
   {
-    accessorKey: "email",
-    header: "Email",
+    accessorKey: "price",
+    header: "Price",
   },
   {
-    accessorKey: "amount",
-    header: "Amount",
+    accessorKey: "isPublished",
+    header: "Published",
   },
 ]
--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -5,25 +5,28 @@ import Link from "next/link";
 import React from "react";
 import { DataTable } from "./_components/DataTable";
 import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
 
-async function getData(): Promise<any[]> {
-	// Fetch data from your API here.
-	return [
-	  {
-		id: "728ed52f",
-		amount: 100,
-		status: "pending",
-		email: "m@example.com",
-	  },
-	  // ...
-	]
-  }
 const CoursesPage = async () => {
-	const data = await getData();
+	const {userId}= auth()
+	if(!userId){
+		return redirect("/")
+	}
+
+	const courses = await db.course.findMany({
+		where:{
+			userId,
+		},
+		orderBy:{
+			createdAt:"desc"
+		}
+	})
 	return (
 		<div className='p-6'>
-			<DataTable columns={columns} data={data} />
+			<DataTable columns={columns} data={courses} />
 			{/* <Link href='/teacher/create'>
 				<Button>New Course</Button>
 			</Link> */}
