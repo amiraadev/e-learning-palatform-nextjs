d665a31904e939c7471dea80b234402a3621d3fb
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-03 19:22:16 +0100

    skeleton of courses table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -3,13 +3,30 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import { DataTable } from "./_components/DataTable";
+import { columns } from "./_components/columns";
 
-const CoursesPage = () => {
+
+async function getData(): Promise<any[]> {
+	// Fetch data from your API here.
+	return [
+	  {
+		id: "728ed52f",
+		amount: 100,
+		status: "pending",
+		email: "m@example.com",
+	  },
+	  // ...
+	]
+  }
+const CoursesPage = async () => {
+	const data = await getData();
 	return (
 		<div className='p-6'>
-			<Link href='/teacher/create'>
+			<DataTable columns={columns} data={data} />
+			{/* <Link href='/teacher/create'>
 				<Button>New Course</Button>
-			</Link>
+			</Link> */}
 		</div>
 	);
 };
