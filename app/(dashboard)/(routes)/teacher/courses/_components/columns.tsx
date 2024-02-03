2d4730b0597221ea6fcaf4d9bb6901bfe5276088
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-03 20:18:08 +0100

    adding the actions to the table throw Dropdown-menu from shadcn

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -1,35 +1,85 @@
-"use client"
+/** @format */
 
-import { Button } from "@/components/ui/button"
-import { Course } from "@prisma/client"
-import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+"use client";
+
+import { Course } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
-
 export const columns: ColumnDef<Course>[] = [
-  {
-    accessorKey: "title",
-    header: ({ column }) => {
-        return (
-          <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            Title
-            <ArrowUpDown className="ml-2 h-4 w-4" />
-          </Button>
-        )
-      },
-  },
-  {
-    accessorKey: "price",
-    header: "Price",
-  },
-  {
-    accessorKey: "isPublished",
-    header: "Published",
-  },
-]
+	{
+		accessorKey: "title",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+					Title
+					<ArrowUpDown className='ml-2 h-4 w-4' />
+				</Button>
+			);
+		},
+	},
+	{
+		accessorKey: "price",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+					Price
+					<ArrowUpDown className='ml-2 h-4 w-4' />
+				</Button>
+			);
+		},
+	},
+	{
+		accessorKey: "isPublished",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+					Published
+					<ArrowUpDown className='ml-2 h-4 w-4' />
+				</Button>
+			);
+		},
+	},
+	{
+		id: "actions",
+		cell: ({ row }) => {
+			const { id } = row.original;
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-4 w-8 p-0">
+                            <span className="sr-only">Open Menu</span>
+                            <MoreHorizontal />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <Link href={`/teacher/courses/${id}`}>
+                            <DropdownMenuItem>
+                                <Pencil className="h-4 w-4 mr-2" />
+                                Edit
+                            </DropdownMenuItem>
+                        </Link>
+                    </DropdownMenuContent>
+				</DropdownMenu>
+			);
+		},
+	},
+];
