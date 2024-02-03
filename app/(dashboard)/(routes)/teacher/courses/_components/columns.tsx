7f1638906dee7fafc0cfe324e753de54a04d49e1
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-03 19:45:43 +0100

    making the title column sortable

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -10,7 +12,17 @@ import { ColumnDef } from "@tanstack/react-table"
 export const columns: ColumnDef<Course>[] = [
   {
     accessorKey: "title",
-    header: "Title",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Title
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
   },
   {
     accessorKey: "price",
