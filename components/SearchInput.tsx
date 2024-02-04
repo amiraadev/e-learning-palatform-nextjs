24dd64376002c0f9a627f5575395e0b4952dd721
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-04 12:07:45 +0100

    using the dbounce hook :  Debouncing is a technique used to delay the execution of a function or, in this case, the update of a value ==> use-dbpounce.tsx

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,21 +1,47 @@
-"use client"
+/** @format */
 
-import { Search } from "lucide-react"
-import { Input } from "./ui/input"
+"use client";
 
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/use-debounce";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 
 const SearchInput = () => {
-  return (
-    <div className="relative">
-        <Search 
-           className="h-4 w-4 absolute top-3 left-3 text-slate-600"
-        />
-        <Input 
-        className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
-        placeholder="Search for a course"
-        />
-    </div>
-  )
-}
+	const [value, setValue] = useState("second");
+	const debouncedValue = useDebounce(value);
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const router = useRouter();
 
-export default SearchInput
+	const currentCategoryId = searchParams.get("categoryId");
+
+	useEffect(() => {
+		const url = qs.stringifyUrl(
+			{
+				url: pathname,
+				query: {
+					categoryId: currentCategoryId,
+					title: debouncedValue,
+				},
+			},
+			{ skipEmptyString: true, skipNull: true }
+		);
+		router.push(url);
+	}, [debouncedValue, router, currentCategoryId, pathname]);
+	return (
+		<div className='relative'>
+			<Search className='h-4 w-4 absolute top-3 left-3 text-slate-600' />
+			<Input
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+				className='w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200'
+				placeholder='Search for a course'
+			/>
+		</div>
+	);
+};
+
+export default SearchInput;
