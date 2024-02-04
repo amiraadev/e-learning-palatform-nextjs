a98b82df687c5d56c53cbd97942d4809aa65428f
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-04 12:24:32 +0100

    getting the progress of the chapters with GetProgress.tsx

---
+++ actions/GetProgress.tsx
@@ -0,0 +1,41 @@
+/** @format */
+
+import React from "react";
+import { db } from "@/lib/db";
+
+const GetProgress = async (
+	userId: string,
+	courseId: string
+): Promise<Number> => {
+	try {
+		const publishedChapters = await db.chapter.findMany({
+			where: {
+				courseId: courseId,
+				isPublished: true,
+			},
+			select: {
+				id: true,
+			},
+		});
+		const publishedChapterIds = publishedChapters.map((chapter) => chapter.id);
+
+		const validCompletedChapters = await db.userProgress.count({
+			where: {
+				userId: userId,
+				chapterId: {
+					in: publishedChapterIds,
+				},
+				isCompleted: true,
+			},
+		});
+
+		const progressPercentage =
+			(validCompletedChapters / publishedChapterIds.length) * 100;
+		return progressPercentage;
+	} catch (error) {
+		console.log("[GET_PROGRESS]", error);
+		return 0;
+	}
+};
+
+export default GetProgress;
--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,27 +1,26 @@
-import { db } from '@/lib/db'
-import React from 'react'
-import Categories from './_components/Categories'
-import SearchInput from '@/components/SearchInput'
+/** @format */
 
-const SearchPAge = async() => {
-  const categories = await db.category.findMany({
-    orderBy:{
-      name:"asc"
-    }
-  })
-  return (
-    <>
-    <div className='px-6 pt-6 md:hidden md:mb-0 block'>
-      <SearchInput/> 
-      
-    </div>
-    <div className='p-6'>
-      <Categories 
-        items={categories}
-      />
-    </div>
-    </>
-  )
-}
+import { db } from "@/lib/db";
+import React from "react";
+import Categories from "./_components/Categories";
+import SearchInput from "@/components/SearchInput";
 
-export default SearchPAge
+const SearchPAge = async () => {
+	const categories = await db.category.findMany({
+		orderBy: {
+			name: "asc",
+		},
+	});
+	return (
+		<>
+			<div className='px-6 pt-6 md:hidden md:mb-0 block'>
+				<SearchInput />
+			</div>
+			<div className='p-6'>
+				<Categories items={categories} />
+			</div>
+		</>
+	);
+};
+
+export default SearchPAge;
