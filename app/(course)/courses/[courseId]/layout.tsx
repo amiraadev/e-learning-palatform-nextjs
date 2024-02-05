25c3b0543f1efd0afd9d0e7a03ee48a8817e8330
yakinmohamed <allagui_fawzi@outlook.fr>
2024-02-05 21:39:23 +0100

    strip.ts

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -1,71 +1,73 @@
-/** @format */
-import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
 import { getProgress } from "@/actions/GetProgress";
-import CourseSidebar from "./_components/CourseSidebar";
-import {CourseNavbar} from "./_components/CourseNavbar";
+
+import  CourseSidebar  from "./_components/CourseSidebar";
+import { CourseNavbar } from "./_components/CourseNavbar";
 
 const CourseLayout = async ({
-	children,
-	params,
+  children,
+  params
 }: {
-	children: React.ReactNode;
-	params: { courseId: string };
+  children: React.ReactNode;
+  params: { courseId: string };
 }) => {
-	const { userId } = auth();
-	if (!userId) {
-		return redirect("/");
-	}
-	const course = await db.course.findUnique({
-		where: {
-			id: params.courseId,
-		},
-		include: {
-			chapters: {
-				where: {
-					isPublished: true,
-				},
-				include: {
-					userProgress: {
-						where: {
-							userId,
-						},
-					},
-				},
-				orderBy: {
-					position: "asc",
-				},
-			},
-		},
-	});
+  const { userId } = auth();
 
-    if(!course){
-        redirect("/");
-    }
+  if (!userId) {
+    return redirect("/")
+  }
 
-    const progressCount = await getProgress(userId,course.id)
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId,
+            }
+          }
+        },
+        orderBy: {
+          position: "asc"
+        }
+      },
+    },
+  });
 
+  if (!course) {
+    return redirect("/");
+  }
 
-	return <div className="h-full">
-		<div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-0">
-			<CourseNavbar
-			course={course}
-			progressCount={progressCount}
-			/>
-		</div>
-        <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-0">
-            <CourseSidebar
-              course={course}
-              progressCount={progressCount}
-            />
-        </div>
-        <main className="md:pl-80 pt-[80px] h-full">
+  const progressCount = await getProgress(userId, course.id);
 
-        </main>
+  return (
+    <div className="h-full">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+        <CourseNavbar
+          course={course}
+          progressCount={progressCount}
+        />
+      </div>
+      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+        <CourseSidebar
+          course={course}
+          progressCount={progressCount}
+        />
+      </div>
+      <main className="md:pl-80 pt-[80px] h-full">
         {children}
-    </div>;
-};
+      </main>
+    </div>
+  )
+}
 
-export default CourseLayout;
+export default CourseLayout
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        success: "bg-emerald-600 text-white hover:bg-emerald-600/80"
       },
       size: {
         default: "h-10 px-4 py-2",
